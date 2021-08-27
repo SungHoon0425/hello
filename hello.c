b8bdd540bf01b976c8e94b4a0e317131292129fa
SungHoon Lee <slee91@huskers.unl.edu>
2021-08-27 00:10:03 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: SungHoon Lee
+ *         slee91@huskers.unl.edu
+ * Date: 2021/08/26
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("SungHoon Lee\n");
+
+  return 0;
+}
